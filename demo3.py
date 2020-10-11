dd90b9af708b5bfb854d35b378398d4974137412
cmy1 <43713534+cmy1@users.noreply.github.com>
2020-10-11 10:46:34 +0800

    Add files via upload

---
+++ demo3.py
@@ -0,0 +1,7 @@
+# python 学习
+# 练习
+# 开发时间： 2020/10/10 21:41
+name = '玛丽亚'
+print(name)
+name = '楚留冰'
+print(name)
---
+++ demo7.py
@@ -0,0 +1,15 @@
+# python 学习
+# 练习
+# 开发时间： 2020/10/10 22:10
+str1 = '人生苦短，我用Python'
+str2 = "人生苦短，我用Python"
+
+str3 = '''人生苦短，
+我用Python'''
+str4 = """人生苦短，
+我用Python"""
+
+print(str1, type(str1))
+print(str2, type(str2))
+print(str3, type(str3))
+print(str4, type(str4))
